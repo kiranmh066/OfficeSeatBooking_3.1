e46af11f9e625335b4034a2e9bad81e4e20e4b26
Prabhu <Prabhu.udagatti@Valtech.com>
2023-03-28 10:24:34 +0530

    3.1 version

---
+++ Office_Seat_Book/Office_Seat_Book_DLL/Repost/FloorRepost.cs
@@ -0,0 +1,49 @@
+﻿using Office_Seat_Book_Entity;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+
+namespace Office_Seat_Book_DLL.Repost
+{
+    public class FloorRepost:IFloorRepost
+    {
+        Office_DB_Context _dbContext;//default private
+
+        public FloorRepost(Office_DB_Context dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        public void AddFloor(Floor floor)
+        {
+            _dbContext.floor.Add(floor);
+            _dbContext.SaveChanges();
+        }
+
+        public void DeleteFloor(int floorId)
+        {
+            var floor = _dbContext.floor.Find(floorId);
+            _dbContext.floor.Remove(floor);
+            _dbContext.SaveChanges();
+        }
+
+        public Floor GetFloorById(int floorId)
+        {
+            return _dbContext.floor.Find(floorId);
+        }
+
+        public IEnumerable<Floor> GetFloors()
+        {
+            return _dbContext.floor.ToList();
+        }
+
+
+        public void UpdateFloor(Floor floor)
+        {
+
+            _dbContext.Entry(floor).State = Microsoft.EntityFrameworkCore.EntityState.Modified;
+            _dbContext.SaveChanges();
+        }
+    }
+}
