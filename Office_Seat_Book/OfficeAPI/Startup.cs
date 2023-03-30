7d2ffcd957b553c4a75109af8921787aa815e061
praveen <praveen.rajendran@valtech.com>
2023-03-30 12:29:25 +0530

    admin

--- Office_Seat_Book/OfficeAPI/Startup.cs
+++ Office_Seat_Book/OfficeAPI/Startup.cs
@@ -26,8 +26,7 @@ namespace OfficeAPI
         }
 
         public IConfiguration Configuration { get; }
-
-        // This method gets called by the runtime. Use this method to add services to the container.
+        
         public void ConfigureServices(IServiceCollection services)
         {
             string connectionStr = Configuration.GetConnectionString("sqlConnection");
