50806d04257cdc9f081128595b88ef5eab8680db
praveen <praveen.rajendran@valtech.com>
2023-04-08 18:08:07 +0530

    praveen

--- Office_Seat_Book/Office_Seat_Book_DLL/Office_DB_Context.cs
+++ Office_Seat_Book/Office_Seat_Book_DLL/Office_DB_Context.cs
@@ -25,7 +25,7 @@ namespace Office_Seat_Book_DLL
 
         protected override void OnConfiguring(DbContextOptionsBuilder dbContextOptionsBuilder)
         {
-            dbContextOptionsBuilder.UseSqlServer("Data Source = DESKTOP-2EN1VVU\\SQLEXPRESS; Initial Catalog = OfficeDa; Integrated Security = True;");
+            dbContextOptionsBuilder.UseSqlServer("Data Source = DESKTOP-2EN1VVU\\SQLEXPRESS; Initial Catalog = OfficeD; Integrated Security = True;");
         }
 
     }
